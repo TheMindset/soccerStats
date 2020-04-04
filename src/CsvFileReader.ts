9f81f5156f21ea28bd128115e90606c6ae3f5450
TheMindset <mulezig2014@outlook.fr>
2020-04-05 00:33:11 +0200

    mapRow method

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -17,17 +17,34 @@ export class CsvFileReader {
     .map((row: string): string[] => {
       return row.split(',')
     })
-    .map((row: string[]): MatchData => {
-      return [
-        dateStringToDate(row[0]),
-        row[1],
-        row[2],
-        +row[3],
-        +row[4],
-        row[5] as MatchResult, // Type assertion ==> Nous disons que cette valeur est soit 'H', 'A', ou 'D'
-        row[6]
-      ]
-    })
-  
+    .map(this.mapRow)
+
+    /**
+     * Au lieu d'appliquer directement la logique.
+     * On exporte la logique dans une méthode qui sera appeler ensuite
+     */
+    // .map((row: string[]): MatchData => {
+    //   return [
+    //     dateStringToDate(row[0]),
+    //     row[1],
+    //     row[2],
+    //     +row[3],
+    //     +row[4],
+    //     row[5] as MatchResult, // Type assertion ==> Nous disons que cette valeur est soit 'H', 'A', ou 'D'
+    //     row[6]
+    //   ]
+    // })
+  }
+
+  mapRow(row: string[]): MatchData {
+    return [
+      dateStringToDate(row[0]),
+      row[1],
+      row[2],
+      +row[3],
+      +row[4],
+      row[5] as MatchResult, // Type assertion ==> Nous disons que cette valeur est soit 'H', 'A', ou 'D'
+      row[6]
+    ]
   }
 }
