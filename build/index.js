f251f6d9e754ae5b997337741b8f7c7a54d9d3e8
TheMindset <mulezig2014@outlook.fr>
2020-04-04 21:47:07 +0200

    Refactoring n°1

--- build/index.js
+++ build/index.js
@@ -11,13 +11,18 @@ var matches = fs_1.default.readFileSync('soccerData.csv', {
     .map(function (row) {
     return row.split(',');
 });
+var matchResult = {
+    HomeWin: 'H',
+    AwayWin: 'A',
+    Draw: 'D'
+};
 var manUnitedWins = 0;
 for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
     var match = matches_1[_i];
-    if (match[1] === 'Man United' && match[5] === 'H') {
+    if (match[1] === 'Man United' && match[5] === matchResult.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === 'Man United' && match[5] === 'A') {
+    else if (match[2] === 'Man United' && match[5] === matchResult.AwayWin) {
         manUnitedWins++;
     }
 }
